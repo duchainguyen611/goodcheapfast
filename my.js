d6270789f3b56edd38f0547d513b79ab2d90ecd1
duchainguyen611 <83647081+duchainguyen611@users.noreply.github.com>
2021-06-01 19:25:47 +0700

    Update my.js

--- my.js
+++ my.js
@@ -2,20 +2,20 @@ let good = document.getElementById("good");
 let cheap = document.getElementById("cheap");
 let fast = document.getElementById("fast");
 
-function uncheckfast(){
-    if(fast.checked == true && good.checked == true){
-        document.getElementById("cheap").checked = false;
-    }
-}
-
 function uncheckcheap(){
-    if(cheap.checked == true && fast.checked == true){
-        document.getElementById("good").checked = false;
+    if(fast.checked == true && good.checked == true){
+        cheap.checked = false;
     }
 }
 
 function uncheckgood(){
+    if(cheap.checked == true && fast.checked == true){
+        good.checked = false;
+    }
+}
+
+function uncheckfast(){
     if(good.checked == true && cheap.checked == true){
-        document.getElementById("fast").checked = false;
+        fast.checked = false;
     }
 }
